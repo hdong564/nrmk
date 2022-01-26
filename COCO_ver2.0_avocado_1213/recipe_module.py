c9bbddd464d0fbe6d65cd23766f6887b05a034d6
hdong564 <hdong564@kaist.ac.kr>
2022-01-26 10:01:58 +0900

    new logic class prototype... done!

--- COCO_ver2.0_avocado_1213/recipe_module.py
+++ COCO_ver2.0_avocado_1213/recipe_module.py
@@ -1,5 +1,6 @@
 
 
+from calendar import SATURDAY
 from telnetlib import STATUS
 from time import time
 from time import sleep
@@ -36,31 +37,52 @@ class next_work():
         recipe = Recipe(RECIPE_DATA_APP[recipe_num-1])
         return (recipe,recipe_num)
 
-    def FposLogic(self,recipe,f_pos):
-        print("Fpos logic processing...")
-        
-        
+    def FposLogic(self,recipe_structure,f_pos):
+        '''Logic for fry pos'''
+        print("Fpos logic processing...", f_pos)
+        recipe = recipe_structure[0]
+        recipe_num = recipe_structure[1]
+        status_f_pos = STATUS_POS[f_pos]
+        if 'fried' in status_f_pos:
+            if (STATUS_FRIED_TIME_UI[f_pos] > recipe.total_time) or EARLY_FIN[f_pos] == True:
+                ''' gotta update'''
+                cmd = cmd_creation.init_cmds()  
+                cmd = cmd_creation.create_cmds()
+                self.todo[0].append(cmd)
 
+        for i in range(1,SHAKING_NUM+1):
+            if "waitshaking{}".format(i) in status_f_pos:
+                for j in range(1,SHAKING_NUM):
+                    if recipe.get_shakeNum() == j:
+                        status = "shaked{}".format(SHAKING_NUM-j+1) + status_f_pos.replace("waitshaking{}_".format(i),"")
+                        break
+        cmd = cmd_creation.init_cmds()
+        cmd = cmd_creation.create_cmds(cmd,None,f_pos,status)
+        self.todo[2].append(cmd)
+        print("********* Fpos logic Done! **********")
 
-    def WposLogic(self,recipe,w_pos):
-        print("     Wpos logic processing...")
-        print("    ",recipe.array)
+    def WposLogic(self,recipe_structure,w_pos):
+        '''Logic for basket pos'''
+        print("Wpos logic processing...")
+        recipe = recipe_structure[0]
+        recipe_num = recipe_structure[1]
+        print(recipe.array)
         menu = recipe.get_menu()
-        print("     menu: ",menu)
+        print("menu: ",menu)
         check_count = 0
         for i in range(6):
             if (2<= i <= 3):
                 continue
             if w_pos == 'w{}'.format(i) or w_pos == 'w{}'.format(i+2):
-                print("     checking: ",'w{}'.format(i),'w{}'.format(i+2))
+                print("checking: ",'w{}'.format(i),'w{}'.format(i+2))
                 f_pos = 'f{}'.format(check_count)
-                print("     ",f_pos)
+                print(f_pos)
             check_count +=1
-        print("     W_pos, f_pos chekcking.. done!", w_pos,f_pos)
+        print("W_pos, f_pos chekcking.. done!", w_pos,f_pos)
     
         # error handling #
         if STATUS_POS[f_pos] != 'nothing' or WAITING_POINT[w_pos] != 'nothing':
-            print("     ERROR! - please remove fry basket")
+            print("ERROR! - please remove fry basket")
             del ORDER_LIST[0]
             ORDER_LIST.append(w_pos)
             # return ?? 
@@ -97,11 +119,11 @@ class next_work():
         for i in self.usable_place_num:
             f_pos = 'f{}'.format(i)
             status_f_pos = STATUS_POS[f_pos]
-            print("checking f_pos ... ",f_pos)
+            #print("checking f_pos ... ",f_pos)
             if status_f_pos != 'nothing':
-                print("Fpos logic Start!")
-                recipe = self.GetRecipe(status_f_pos)[0]
-                self.FposLogic(recipe,f_pos)    
+                print("******** Fpos logic Start! *********")
+                recipe_structure = self.GetRecipe(status_f_pos)
+                self.FposLogic(recipe_structure,f_pos)    
             else:
                 continue
 
@@ -112,8 +134,8 @@ class next_work():
             if status_w_pos != 'nothing':
                 print(status_w_pos)
                 print("######### Wpos logic Start! #########")
-                recipe = self.GetRecipe(status_w_pos)[0]
-                self.WposLogic(recipe,w_pos)
+                recipe_structure = self.GetRecipe(status_w_pos)
+                self.WposLogic(recipe_structure,w_pos)
 
     def __del__(self):
         print(f"delete {self.name}")
